db57ca4f439382e8ad3436f17fc880162cc33421
Mr. Dariush Tasdighi <DariushT@GMail.com>
2023-07-13 16:38:28 +0330

    Making some changes!

--- Application/File.cs
+++ Application/File.cs
@@ -5,25 +5,37 @@
 /// </summary>
 public class File
 {
+	#region Constructor
 	/// <summary>
 	/// Constructor
 	/// </summary>
 	public File()
 	{
 	}
+	#endregion /Constructor
 
+	#region Properties
+
+	#region public int Size { get; set; }
 	/// <summary>
 	/// Size
 	/// </summary>
 	public int Size { get; set; }
+	#endregion /public int Size { get; set; }
 
+	#region public string Name { get; set; }
 	/// <summary>
 	/// Name
 	/// </summary>
 	public string Name { get; set; }
+	#endregion /public string Name { get; set; }
 
+	#region public Enums.FileAttributes Attributes { get; set; }
 	/// <summary>
 	/// File Attributes
 	/// </summary>
 	public Enums.FileAttributes Attributes { get; set; }
+	#endregion /public Enums.FileAttributes Attributes { get; set; }
+
+	#endregion /Properties
 }
--- Application/Person.cs
+++ Application/Person.cs
@@ -5,25 +5,37 @@
 /// </summary>
 public class Person
 {
+	#region Constructor
 	/// <summary>
 	/// Constructor
 	/// </summary>
 	public Person()
 	{
 	}
+	#endregion /Constructor
 
+	#region Properties
+
+	#region public string FullName { get; set; }
 	/// <summary>
 	/// Full Name
 	/// </summary>
 	public string FullName { get; set; }
+	#endregion /public string FullName { get; set; }
 
+	#region public Enums.Gender Gender { get; set; }
 	/// <summary>
 	/// Gender
 	/// </summary>
 	public Enums.Gender Gender { get; set; }
+	#endregion /public Enums.Gender Gender { get; set; }
 
+	#region public Enums.Marriage Marriage { get; set; }
 	/// <summary>
 	/// Marriage
 	/// </summary>
 	public Enums.Marriage Marriage { get; set; }
+	#endregion /public Enums.Marriage Marriage { get; set; }
+
+	#endregion /Properties
 }
